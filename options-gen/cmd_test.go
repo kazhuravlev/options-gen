733b4f130ffcad5f9105ad8b453f750738bc775b
Kirill Zhuravlev <kazhuravlev@fastmail.com>
2022-08-16 00:53:19 +0400

    ++

--- options-gen/cmd_test.go
+++ options-gen/cmd_test.go
@@ -13,37 +13,54 @@ import (
 func TestRun(t *testing.T) {
 	t.Parallel()
 
-	var testDirs []string
-	{
-		const testdataDir = "./testdata"
-		fileInfos, err := ioutil.ReadDir(testdataDir)
-		require.NoError(t, err)
+	t.Run("simple_cases", func(t *testing.T) {
+		t.Parallel()
 
-		for _, file := range fileInfos {
-			if !file.IsDir() {
-				continue
+		var testDirs []string
+		{
+			const testdataDir = "./testdata"
+			fileInfos, err := ioutil.ReadDir(testdataDir)
+			require.NoError(t, err)
+
+			for _, file := range fileInfos {
+				if !file.IsDir() {
+					continue
+				}
+
+				testDirs = append(testDirs, filepath.Join(testdataDir, file.Name()))
 			}
-
-			testDirs = append(testDirs, filepath.Join(testdataDir, file.Name()))
 		}
-	}
 
-	for _, dir := range testDirs { //nolint:paralleltest
-		dir := dir
-		t.Run(dir, func(t *testing.T) {
-			outFilename := filepath.Join(dir, "options_generated.go")
-			expFilename := filepath.Join(dir, "options_generated.go.expected")
-			err := optionsgen.Run(
-				filepath.Join(dir, "options.go"),
-				outFilename,
-				"Options",
-				"testcase",
-			)
-			assert.NoError(t, err)
+		for _, dir := range testDirs {
+			dir := dir
+			t.Run(dir, func(t *testing.T) {
+				outFilename := filepath.Join(dir, "options_generated.go")
+				expFilename := filepath.Join(dir, "options_generated.go.expected")
+				err := optionsgen.Run(
+					filepath.Join(dir, "options.go"),
+					outFilename,
+					"Options",
+					"testcase",
+				)
+				assert.NoError(t, err)
 
-			helpEqualFiles(t, expFilename, outFilename)
-		})
-	}
+				helpEqualFiles(t, expFilename, outFilename)
+			})
+		}
+	})
+
+	t.Run("soource_not_found", func(t *testing.T) {
+		t.Parallel()
+
+		dir := t.TempDir()
+		err := optionsgen.Run(
+			filepath.Join(dir, "options.go"),
+			filepath.Join(dir, "options_generated.go"),
+			"Options",
+			"testcase",
+		)
+		assert.Error(t, err)
+	})
 }
 
 func helpEqualFiles(t *testing.T, filename1, filename2 string) {
