5a51fadcfb46c8f56b745cdbf7015c270532000d
Kirill Zhuravlev <kazhuravlev@fastmail.com>
2024-01-20 22:04:43 +0100

    upd example

--- examples/go-generate-2options-1pkg/options.go
+++ examples/go-generate-2options-1pkg/options.go
@@ -1,6 +1,6 @@
 package gogenerate
 
-//go:generate options-gen -from-struct=Options1 -out-prefix=KKK -out-filename=options1_generated.go
+//go:generate options-gen -from-struct=Options1 -out-prefix=KKK -out-filename=options1_generated.go -defaults-from=var=defaultOptions1
 type Options1 struct {
 	// Options1.field0
 	field0 int `validate:"min:3"`
@@ -12,7 +12,14 @@ type Options1 struct {
 	field3 int `validate:"min:3"`
 }
 
-//go:generate options-gen -from-struct=Options2 -out-prefix=NNN -out-filename=options2_generated.go
+var defaultOptions1 = Options1{
+	field0: 0,
+	field1: 1,
+	field2: 2,
+	field3: 3,
+}
+
+//go:generate options-gen -from-struct=Options2 -out-prefix=NNN -out-filename=options2_generated.go -defaults-from=var=defaultOptions2
 type Options2 struct {
 	// Options2.field1
 	field1 int `validate:"min:3"`
@@ -22,4 +29,11 @@ type Options2 struct {
 	field3 int `validate:"min:3"`
 	// Options2.field4
 	field4 int `validate:"min:3"`
+}
+
+var defaultOptions2 = Options2{
+	field1: 1,
+	field2: 2,
+	field3: 3,
+	field4: 4,
 }
--- examples/go-generate-2options-1pkg/options2_generated.go
+++ examples/go-generate-2options-1pkg/options2_generated.go
@@ -8,14 +8,18 @@ import (
 	validator461e464ebed9 "github.com/kazhuravlev/options-gen/pkg/validator"
 )
 
-type OptNNNOptions2Setter func(o *Options2)
+type OptOptions2Setter func(o *Options2)
 
 func NewOptions2(
-	options ...OptNNNOptions2Setter,
+	options ...OptOptions2Setter,
 ) Options2 {
 	o := Options2{}
 
-	// Setting defaults from field tag (if present)
+	// Setting defaults from variable
+	o.field1 = defaultOptions2.field1
+	o.field2 = defaultOptions2.field2
+	o.field3 = defaultOptions2.field3
+	o.field4 = defaultOptions2.field4
 
 	for _, opt := range options {
 		opt(&o)
@@ -24,28 +28,28 @@ func NewOptions2(
 }
 
 // Options2.field1
-func WithNNNField1(opt int) OptNNNOptions2Setter {
+func WithNNNField1(opt int) OptOptions2Setter {
 	return func(o *Options2) {
 		o.field1 = opt
 	}
 }
 
 // Options2.field2
-func WithNNNField2(opt int) OptNNNOptions2Setter {
+func WithNNNField2(opt int) OptOptions2Setter {
 	return func(o *Options2) {
 		o.field2 = opt
 	}
 }
 
 // Options2.field3
-func WithNNNField3(opt int) OptNNNOptions2Setter {
+func WithNNNField3(opt int) OptOptions2Setter {
 	return func(o *Options2) {
 		o.field3 = opt
 	}
 }
 
 // Options2.field4
-func WithNNNField4(opt int) OptNNNOptions2Setter {
+func WithNNNField4(opt int) OptOptions2Setter {
 	return func(o *Options2) {
 		o.field4 = opt
 	}
@@ -53,35 +57,35 @@ func WithNNNField4(opt int) OptNNNOptions2Setter {
 
 func (o *Options2) Validate() error {
 	errs := new(errors461e464ebed9.ValidationErrors)
-	errs.Add(errors461e464ebed9.NewValidationError("field1", _validate_NNNOptions2_field1(o)))
-	errs.Add(errors461e464ebed9.NewValidationError("field2", _validate_NNNOptions2_field2(o)))
-	errs.Add(errors461e464ebed9.NewValidationError("field3", _validate_NNNOptions2_field3(o)))
-	errs.Add(errors461e464ebed9.NewValidationError("field4", _validate_NNNOptions2_field4(o)))
+	errs.Add(errors461e464ebed9.NewValidationError("field1", _validate_Options2_field1(o)))
+	errs.Add(errors461e464ebed9.NewValidationError("field2", _validate_Options2_field2(o)))
+	errs.Add(errors461e464ebed9.NewValidationError("field3", _validate_Options2_field3(o)))
+	errs.Add(errors461e464ebed9.NewValidationError("field4", _validate_Options2_field4(o)))
 	return errs.AsError()
 }
 
-func _validate_NNNOptions2_field1(o *Options2) error {
+func _validate_Options2_field1(o *Options2) error {
 	if err := validator461e464ebed9.GetValidatorFor(o).Var(o.field1, "min:3"); err != nil {
 		return fmt461e464ebed9.Errorf("field `field1` did not pass the test: %w", err)
 	}
 	return nil
 }
 
-func _validate_NNNOptions2_field2(o *Options2) error {
+func _validate_Options2_field2(o *Options2) error {
 	if err := validator461e464ebed9.GetValidatorFor(o).Var(o.field2, "min:3"); err != nil {
 		return fmt461e464ebed9.Errorf("field `field2` did not pass the test: %w", err)
 	}
 	return nil
 }
 
-func _validate_NNNOptions2_field3(o *Options2) error {
+func _validate_Options2_field3(o *Options2) error {
 	if err := validator461e464ebed9.GetValidatorFor(o).Var(o.field3, "min:3"); err != nil {
 		return fmt461e464ebed9.Errorf("field `field3` did not pass the test: %w", err)
 	}
 	return nil
 }
 
-func _validate_NNNOptions2_field4(o *Options2) error {
+func _validate_Options2_field4(o *Options2) error {
 	if err := validator461e464ebed9.GetValidatorFor(o).Var(o.field4, "min:3"); err != nil {
 		return fmt461e464ebed9.Errorf("field `field4` did not pass the test: %w", err)
 	}
