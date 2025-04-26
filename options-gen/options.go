8a4143375df7c34636463a3beccefa591f757696
Kirill Zhuravlev <kazhuravlev@fastmail.com>
2025-04-26 22:04:38 +0200

    add options for Run

---
+++ options-gen/options.go
@@ -0,0 +1,17 @@
+package optionsgen
+
+//go:generate toolset run options-gen -from-struct=Options
+type Options struct {
+	version               string `validate:"required"`
+	inFilename            string `validate:"required"`
+	outFilename           string `validate:"required"`
+	structName            string `validate:"required"`
+	packageName           string `validate:"required"`
+	outPrefix             string
+	defaults              Defaults `validate:"required"`
+	showWarnings          bool
+	withIsset             bool
+	allVariadic           bool
+	constructorTypeRender ConstructorTypeRender `validate:"required"`
+	outOptionTypeName     string
+}
