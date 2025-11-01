1a7acc9314ac38041956223cfab8274a7974089e
Kirill Zhuravlev <kazhuravlev@fastmail.com>
2025-11-01 23:43:05 +0100

    improve version algorithm

--- internal/version/version.go
+++ internal/version/version.go
@@ -10,13 +10,15 @@ var version = versionUnknown
 
 func GetVersion() string {
 	// In case if not - someone (task examples:update) explicitly set the value of version.
-	if version == versionUnknown {
-		if bi, ok := debug.ReadBuildInfo(); ok {
-			if bi.Main.Version != "" {
-				return bi.Main.Version
-			}
+	if version != versionUnknown {
+		return version
+	}
+
+	if bi, ok := debug.ReadBuildInfo(); ok {
+		if bi.Main.Version != "" {
+			return bi.Main.Version
 		}
 	}
 
-	return version
+	return versionUnknown
 }
