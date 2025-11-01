ba2983294733226d184aad2a827006460264b32b
Kirill Zhuravlev <kazhuravlev@fastmail.com>
2025-11-02 00:58:03 +0100

    add version test

---
+++ internal/version/version_test.go
@@ -0,0 +1,22 @@
+package version
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestGetVersion(t *testing.T) {
+	require.Equal(t, "(devel)", GetVersion())
+
+	t.Run("returns explicitly set version when version variable is set", func(t *testing.T) {
+		// Save original value
+		original := version
+		defer func() { version = original }()
+
+		// Set explicit version
+		version = "v1.2.3"
+		assert.Equal(t, "v1.2.3", GetVersion())
+	})
+}
